9543f57654c963c6e3af13adebc7949a12e0b5dc
omarovici <omarkhalidsaber042004@gmail.com>
2025-04-19 18:03:09 +0200

    Add initial migration and setup for To-Do List project

---
+++ To-Do-List/Models/Task.cs
@@ -0,0 +1,11 @@
+namespace To_Do_List_Project.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+    public string Priority { get; set; } = "medium";
+}
